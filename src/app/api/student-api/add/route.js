6e1ac52798ef8f9979ae5cb3ff3996d82cc119e2
Kittiwin Phannachet <kittiwinktb@gmail.com>
2025-01-18 12:04:54 +0700

    update master schema

--- src/app/api/student-api/add/route.js
+++ src/app/api/student-api/add/route.js
@@ -4,19 +4,8 @@ export async function POST(req) {
   try {
     const { student_id, faculty, student_name } = await req.json();
 
-    // // Check if the course exists
-    // const existingCourse = await prisma.course.findUnique({
-    //   where: { course_id },
-    // });
-    // if (!existingCourse) {
-    //   return new Response(
-    //     JSON.stringify({ error: "Course does not exist." }),
-    //     { status: 400 }
-    //   );
-    // }
-
     // Mock teacher_id for now (replace with actual session logic later)
-    const teacher_id = 1; // Replace with actual logic when auth is implemented
+    const teacher_id = 1; 
 
     const teacher = await prisma.user.findUnique({
         where: { id: teacher_id },
@@ -56,31 +45,6 @@ export async function POST(req) {
         },
       });
     }
-
-    // // Check if the student is already in the course
-    // const existingCourseStudent = await prisma.course_student.findFirst({
-    //   where: {
-    //     course_id,
-    //     student_id,
-    //   },
-    // });
-
-    // if (existingCourseStudent) {
-    //   return new Response(
-    //     JSON.stringify({
-    //       error: "Student is already enrolled in this course.",
-    //     }),
-    //     { status: 400 }
-    //   );
-    // }
-
-    // // Add the student to the course
-    // const addToCourseStudent = await prisma.course_student.create({
-    //   data: {
-    //     course_id,
-    //     student_id: existingStudent.student_id,
-    //   },
-    // });
 
     return new Response(
       JSON.stringify({ student: existingStudent }),
