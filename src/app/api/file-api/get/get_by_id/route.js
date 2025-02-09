fb3bb838d9f20c711d38b69f4358c3a88e2587fe
Kittiwin Phannachet <kittiwinktb@gmail.com>
2025-02-09 15:04:27 +0700

    add get file by course_id and section

--- src/app/api/file-api/get/get_by_id/route.js
+++ src/app/api/file-api/get/get_by_id/route.js
@@ -1,27 +1,28 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../../../prisma/prisma";
 
-// http://localhost:3000/api/file-api/get/get_by_id?course_id=001001
+// http://localhost:3000/api/file-api/get/get_by_id?course_id=001001&section=001
 export async function GET(req) {
   try {
     const { searchParams } = new URL(req.url);
     const courseId = searchParams.get("course_id");
+    const section = searchParams.get("section");
 
-    if (!courseId) {
+    if (!courseId || !section) {
       return NextResponse.json(
-        { error: "Course ID is required" },
+        { error: "Course ID and section are required" },
         { status: 400 }
       );
     }
 
     // Fetch files by course_id
     const files = await prisma.file.findMany({
-      where: { course_id: courseId },
+      where: { course_id: courseId, section: section },
     });
 
     if (files.length === 0) {
       return NextResponse.json(
-        { message: `No files found for course_id ${courseId}` },
+        { message: `No files found for course_id ${courseId} of section ${section}` },
         { status: 404 }
       );
     }
