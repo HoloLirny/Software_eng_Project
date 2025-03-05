9499b3e7790e4ef95b7be77b7cdef65b33b9c92f
Kittiwin Phannachet <kittiwinktb@gmail.com>
2025-03-05 14:58:53 +0700

    make get_student_history

--- src/app/api/attendance-api/get/get_student_history/route.js
+++ src/app/api/attendance-api/get/get_student_history/route.js
@@ -1,16 +1,15 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../../../prisma/prisma";
 
-// http://localhost:3000/api/attendance-api/get/get_student_history?student_id=650610000&course_id=261361&user_email=teacher@example.com
+// http://localhost:3000/api/attendance-api/get/get_student_history?student_id=650610000&user_email=teacher@example.com
 
 export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url);
     const student_id = searchParams.get("student_id");
-    const course_id = searchParams.get("course_id");
     const user_email = searchParams.get("user_email");
 
-    if (!course_id || !user_email|| !student_id) {
+    if (!user_email|| !student_id) {
       return NextResponse.json(
         { error: "Missing course_id and user email parameter" },
         { status: 400 }
@@ -39,22 +38,8 @@ export async function GET(request) {
       );
     }
 
-    const exitingcouse = await prisma.course.findFirst({
-      where: {
-        course_id: course_id,
-      },
-    });
-
-    if (!exitingcouse) {   
-        return NextResponse.json(
-            { error: "Course not found" },
-            { status: 404 }
-        );
-    }
-
     const attendance = await prisma.attendance.findMany({
       where: {
-        course_id: course_id, 
         student_id: student_id,
       },
       include: {
